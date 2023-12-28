eb5f1791f32227ca33ba573ea51e2d9b65ad9495
crisunk <cristiano.unk@gmail.com>
2023-12-28 16:17:33 -0300

    implementação do prismic

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
+
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = 3002
 
 const Prismic = require('@prismicio/client')
 const PrismicH = require('@prismicio/helpers')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
 
-// Typo correction: Prismic.Predicates.at instead of Prismic.Predicate.at
 const { PrismicPredicates } = Prismic
 
 function initApi(req) {
@@ -38,23 +39,100 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 app.locals.basedir = app.get('views')
 
+// app.get('/', async (req, res) => {
+// 	res.render('pages/home');
+//   });
+
+// app.get('/home', async (req, res) => {
+// 	initApi(req).then(api => {
+// 		api.query(
+// 			Prismic.Predicates.at('document.type', 'home')
+// 		).then(response => {
+// 			const {results} = response
+
+// 			console.log(results)
+
+// 			res.render('pages/home')
+// 		})
+// 	})
+// })
+
+// app.get('/', async (req, res) => {
+//   const api = await initApi(req)
+//   const home = await api.getSingle('about')
+
+//   res.render('pages/home', { home: home.data })
+
+
+// })
+
+// app.get('/', async (req, res) => {
+//   const api = await initApi(req)
+//   const home = await api.getSingle('intro', 'project', 'misc', 'about' )
+// 		console.log(home)
+//   res.render('pages/home', { home });
+// })
+
+// app.get('/', async (req, res) => {
+//   const api = await initApi(req)
+//   const home = await
+//     Promise.all([
+//         api.getSingle('intro'),
+//         api.getSingle('project'),
+//         api.getSingle('misc'),
+//         api.getSingle('about')
+//         )];
+// 		console.log(home)
+//   res.render('pages/home', { home })
+// })
+
 app.get('/', async (req, res) => {
-	res.render('pages/home');
-  });
+    const api = await initApi(req)
+    const intro = await api.getSingle('intro')
+    const project = await api.getSingle('project')
+    const misc = await api.getSingle('misc')
+    const about = await api.getSingle('about')
+    const home = [intro, project, misc, about]
+    console.log(home)
+    res.render('pages/home', { home })
+    })
 
 
-const handleRequest = async (api) => {
-	const [about, intro, meta_data, misc, project, project_cover, { results: collection }] = await Promise.all([
-		api.getSingle('intro'),
-		api.getSingle('project'),
-		api.getSingle('misc'),
-		api.getSingle('about'),
-		api.query(Prismic.Predicates.at('document.type', 'collection'), {
-			fetchLinks: 'product.image',
-		}),
-	])
+
+// 	const handleRequest = async (api) => {
+//     try {
+//         const [about, intro, meta_data, misc, project, project_cover, { results: collection }] = await Promise.all([
+//             api.getSingle('intro'),
+//             api.getSingle('project'),
+//             api.getSingle('meta_data'),
+//             api.getSingle('misc'),
+//             api.getSingle('about'),
+//             api.query(Prismic.Predicates.at('document.type', 'collection'), {
+//                 fetchLinks: 'product.image',
+//             }),
+//         ]);
+//         // Resto do seu código de manipulação dos dados
+//     } catch (error) {
+//         console.error("Erro ao lidar com as requisições Prismic:", error);
+//         // Lógica para lidar com o erro, como retornar uma mensagem de erro ou tratar de outra forma
+//     }
+// }
+
+// código velho
+
+// const handleRequest = async (api) => {
+// 	const [about, intro, meta_data, misc, project, project_cover, { results: collection }] = await Promise.all([
+// 		api.getSingle('intro'),
+// 		api.getSingle('project'),
+// 		api.getSingle('meta_data'),
+// 		api.getSingle('misc'),
+// 		api.getSingle('about'),
+// 		api.query(Prismic.Predicates.at('document.type', 'collection'), {
+// 			fetchLinks: 'product.image',
+// 		}),
+// 	])
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 })
-}
+// }
