5c79f4b5659756e62880fd96e2ca9b32259f1c64
crisunk <cristiano.unk@gmail.com>
2023-12-29 22:15:35 -0300

    implementação prismic

--- app.js
+++ app.js
@@ -4,135 +4,78 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3002
+const port = 3000
+
 
 const Prismic = require('@prismicio/client')
 const PrismicH = require('@prismicio/helpers')
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
 
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch,
+  })
+}
+
 const { PrismicPredicates } = Prismic
-
-function initApi(req) {
-	return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
-		accessToken: process.env.PRISMIC_ACESS_TOKEN,
-		req,
-		fetch,
-	})
-}
-
-const HandleLinkResolver = (doc) => {
-	return '/';
-}
-
-
-app.use((req, res, next) => {
-	res.locals.ctx = {
-		endpoint: process.env.PRISMIC_ENDPOINT,
-		linkResolver: HandleLinkResolver,
-	}
-	res.locals.PrismicH = PrismicH
-
-	next()
-})
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 app.locals.basedir = app.get('views')
 
-// app.get('/', async (req, res) => {
-// 	res.render('pages/home');
-//   });
+// Link Resolver
+const HandleLinkResolver = (doc) => {
+  // if (doc.type === 'product') {
+  //   return `/detail/${doc.slug}`
+  // }
 
-// app.get('/home', async (req, res) => {
-// 	initApi(req).then(api => {
-// 		api.query(
-// 			Prismic.Predicates.at('document.type', 'home')
-// 		).then(response => {
-// 			const {results} = response
+  // if (doc.type === 'collections') {
+  //   return '/collections'
+  // }
 
-// 			console.log(results)
+  // if (doc.type === 'about') {
+  //   return `/about`
+  // }
 
-// 			res.render('pages/home')
-// 		})
-// 	})
-// })
+  // Default to homepage
+  return '/'
+}
 
-// app.get('/', async (req, res) => {
-//   const api = await initApi(req)
-//   const home = await api.getSingle('about')
-
-//   res.render('pages/home', { home: home.data })
-
-
-// })
-
-// app.get('/', async (req, res) => {
-//   const api = await initApi(req)
-//   const home = await api.getSingle('intro', 'project', 'misc', 'about' )
-// 		console.log(home)
-//   res.render('pages/home', { home });
-// })
-
-// app.get('/', async (req, res) => {
-//   const api = await initApi(req)
-//   const home = await
-//     Promise.all([
-//         api.getSingle('intro'),
-//         api.getSingle('project'),
-//         api.getSingle('misc'),
-//         api.getSingle('about')
-//         )];
-// 		console.log(home)
-//   res.render('pages/home', { home })
-// })
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.locals.basedir = app.get('views');
 
 app.get('/', async (req, res) => {
-    const api = await initApi(req)
-    const intro = await api.getSingle('intro')
-    const project = await api.getSingle('project')
-    const misc = await api.getSingle('misc')
-    const about = await api.getSingle('about')
-    const home = [intro, project, misc, about]
-    console.log(home)
-    res.render('pages/home', { home })
-    })
+  const api = await initApi(req)
+  const misc = await api.getSingle('misc')
+
+  res.render('pages/home', { home: misc.data.gallery})
+	console.log(misc.data.gallery)
 
 
-
-// 	const handleRequest = async (api) => {
-//     try {
-//         const [about, intro, meta_data, misc, project, project_cover, { results: collection }] = await Promise.all([
-//             api.getSingle('intro'),
-//             api.getSingle('project'),
-//             api.getSingle('meta_data'),
-//             api.getSingle('misc'),
-//             api.getSingle('about'),
-//             api.query(Prismic.Predicates.at('document.type', 'collection'), {
-//                 fetchLinks: 'product.image',
-//             }),
-//         ]);
-//         // Resto do seu código de manipulação dos dados
-//     } catch (error) {
-//         console.error("Erro ao lidar com as requisições Prismic:", error);
-//         // Lógica para lidar com o erro, como retornar uma mensagem de erro ou tratar de outra forma
-//     }
-// }
-
-// código velho
-
-// const handleRequest = async (api) => {
-// 	const [about, intro, meta_data, misc, project, project_cover, { results: collection }] = await Promise.all([
-// 		api.getSingle('intro'),
-// 		api.getSingle('project'),
-// 		api.getSingle('meta_data'),
-// 		api.getSingle('misc'),
-// 		api.getSingle('about'),
-// 		api.query(Prismic.Predicates.at('document.type', 'collection'), {
-// 			fetchLinks: 'product.image',
-// 		}),
-// 	])
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
 })
-// }
+
+// const assets = []
+
+// home.data.gallery.forEach(item => {
+// 	assets.push(item.image.url)
+// })
+
+
+app.get('/', async (req, res) => {
+	const api = await initApi(req)
+	const intro = await api.getSingle('intro')
+	const project = await api.getSingle('project')
+	const misc = await api.getSingle('misc')
+	const about = await api.getSingle('about')
+	const home = [intro, project, misc, about]
+	console.log(home)
+	res.render('pages/home', { home })
+	})
+
+	app.listen(port, () => {
+		console.log(`http://localhost:${port}`);
+})
+
